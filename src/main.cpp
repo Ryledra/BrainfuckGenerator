982c2b2cc3dc3e648703b5eb6c1bfcb9e2289a3e
Vince Martin <vince@nadin.one>
2024-09-01 12:56:13 +0100

    initial BF generation using only +/- added

--- src/main.cpp
+++ src/main.cpp
@@ -7,26 +7,42 @@ class X {
 
     public:
         X(char* input);
-        void print();
+        void printValue();
+        void bfValue(unsigned char mem);
 };
 
 X::X(char* input)   {
     // std::cout << *input;
-    if (!*input) { return; }
+    if (!*input) return;
     value = (unsigned char)(* input);
     next = new X(++input);
 };
 
-void X::print()    {
-    if (!value) { return; }
+void X::printValue()    {
+    if (!value) return;
     std::cout << value;
-    (*next).print();
+    (*next).printValue();
+};
+
+void X::bfValue(unsigned char mem)  {
+    if (!value) return;
+    int itt = { (mem < value) ? 1 : -1 };
+
+    while (mem != value) {
+        mem += itt;
+        std::cout << ((itt == 1) ? '+' : '-');
+    }
+
+    std::cout << '.';
+    (*next).bfValue(mem);
 };
 
 int main(int argc, char *argv[])  {
     if (argc != 2) return -1;
     std::cout << argv[1] << "\n";
     X str = { X( argv[1] ) };
-    str.print();
+    str.printValue();
+    std::cout << "\n";
+    str.bfValue((unsigned char)0);
     return 0;
 }
